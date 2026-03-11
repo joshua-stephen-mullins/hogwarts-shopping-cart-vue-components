29d20ad572085a1f1de7a6e252c64a0f9587f423
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2026-03-11 17:15:51 -0500

    Completed video 21

---
+++ src/components/CartList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <CartListItem></CartListItem>
+        <CartListItem></CartListItem>
+    </div>
+</template>
+
+<script setup>
+import CartListItem from '@/components/CartListItem.vue';
+
+</script>
+
+<style scoped></style>
