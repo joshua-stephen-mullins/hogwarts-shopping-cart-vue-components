77e958272f1716d2fc99ea3107e009b1bdce7f9d
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2026-03-13 21:11:52 -0500

    Completed video 24

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -2,7 +2,8 @@
     <div>
         <CartTitle :username></CartTitle>
         <div class="cart-container">
-            <CartList class="cart-list" :cart-items="shoppingCartItems"></CartList>
+            <CartList class="cart-list" :cart-items="shoppingCartItems" @item-remove="removeItem($event)"
+                @quantity-update="updateQuantity($event)"></CartList>
             <OrderSummary class="order-summary" :cart-items="shoppingCartItems"></OrderSummary>
         </div>
     </div>
@@ -10,7 +11,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { watch, ref } from 'vue'
 
 import CartTitle from '@/components/CartTitle.vue';
 import CartList from '@/components/CartList.vue';
@@ -18,47 +19,66 @@ import OrderSummary from '@/components/OrderSummary.vue';
 
 let username = ref('Harry')
 let shoppingCartItems = ref([
-  {
-    id: 1,
-    productName: 'Dragon Liver',
-    price: 1500,
-    isInStock: true,
-    quantity: 3,
-    image: 'src/assets/img/DragonLiver.png'
-  },
-  {
-    id: 2,
-    productName: 'Golden Snitch',
-    price: 600,
-    isInStock: true,
-    quantity: 2,
-    image: ' src/assets/img/GoldenSnitch.png'
-  },
-  {
-    id: 3,
-    productName: 'Unicorn Tail Hair',
-    price: 1200,
-    isInStock: false,
-    quantity: 1,
-    image: 'src/assets/img/UnicornTailHair.png'
-  },
-  {
-    id: 4,
-    productName: 'Wand',
-    price: 2000,
-    isInStock: true,
-    quantity: 1,
-    image: 'src/assets/img/Wand.jpg'
-  },
-  {
-    id: 5,
-    productName: 'Nimbus 2000',
-    price: 5000,
-    isInStock: true,
-    quantity: 1,
-    image: 'src/assets/img/Nimbus2000.jpg'
-  }
+    {
+        id: 1,
+        productName: 'Dragon Liver',
+        price: 1500,
+        isInStock: true,
+        quantity: 3,
+        image: 'src/assets/img/DragonLiver.png'
+    },
+    {
+        id: 2,
+        productName: 'Golden Snitch',
+        price: 600,
+        isInStock: true,
+        quantity: 2,
+        image: ' src/assets/img/GoldenSnitch.png'
+    },
+    {
+        id: 3,
+        productName: 'Unicorn Tail Hair',
+        price: 1200,
+        isInStock: false,
+        quantity: 1,
+        image: 'src/assets/img/UnicornTailHair.png'
+    },
+    {
+        id: 4,
+        productName: 'Wand',
+        price: 2000,
+        isInStock: true,
+        quantity: 1,
+        image: 'src/assets/img/Wand.jpg'
+    },
+    {
+        id: 5,
+        productName: 'Nimbus 2000',
+        price: 5000,
+        isInStock: true,
+        quantity: 1,
+        image: 'src/assets/img/Nimbus2000.jpg'
+    }
 ])
+
+function removeItem(id) {
+    let index = shoppingCartItems.value.findIndex(item => item.id == id)
+    shoppingCartItems.value.splice(index, 1)
+}
+
+function updateQuantity(val) {
+    const { id, newQuantity } = val
+    shoppingCartItems.value.some((item) => {
+        if (item.id == id) {
+            item.quantity = parseInt(newQuantity)
+            return true
+        }
+    })
+}
+
+watch(shoppingCartItems, () => {
+    localStorage.setItem('hogwartsShoppingCart', JSON.stringify(shoppingCartItems.value))
+}, { deep: true })
 
 </script>
 
